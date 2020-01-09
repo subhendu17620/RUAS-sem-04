783cd989660b1ded31d6c51546a595cc6748ced3
subhendu17620 <subhendu17620@gmail.com>
2020-01-09 12:40:40 +0530

    eulerMethod-plot

--- MATLAB/eulerMethod.m
+++ MATLAB/eulerMethod.m
@@ -1,12 +1,16 @@
-function [ ] = eulerMethod(x0,y0,xn,h,f)
+function [ ] = eulerMethod(x0,y0,xn,h)
 x=x0:h:xn;
 n=length(x);
 y(1)=y0;
+f=@(x,y) (x+2*y);
 
 for i=2:n
     y(i)=y(i-1)+h*f(x(i-1),y(i-1));
 end
 fprintf('the approximate value of y at x is y(%f)=%f \n',x(end),y(end));
-plot(x,y)
+
+z=eval(dsolve('Dy=x+2*y','y(0)=1','x'));
+plot(x,y,'r',x,z,'k');
 end
 
+    
