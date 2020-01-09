01bb8671d0e0eb39ee5ae60afc504e003c165d65
subhendu17620 <subhendu17620@gmail.com>
2020-01-09 16:20:09 +0530

    update

--- MATLAB/eulerMethod.m
+++ MATLAB/eulerMethod.m
@@ -1,3 +1,5 @@
+%solve dy/dx = x+2y , y(0)=1 , using Euler's Method by taking h=0.1 and
+%find y(0.4) ???
 function [ ] = eulerMethod(x0,y0,xn,h)
 x=x0:h:xn;
 n=length(x);
@@ -11,6 +13,7 @@ fprintf('the approximate value of y at x is y(%f)=%f \n',x(end),y(end));
 
 z=eval(dsolve('Dy=x+2*y','y(0)=1','x'));
 plot(x,y,'r',x,z,'k');
+legend('Approximate Value','Exact Value');
 end
 
     
