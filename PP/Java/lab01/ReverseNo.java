4014c20069616fae110c7fc4c211723ebf4acad1
subhendu17620 <subhendu17620@gmail.com>
2020-01-17 11:21:49 +0530

    update

--- PP/Java/lab01/ReverseNo.java
+++ PP/Java/lab01/ReverseNo.java
@@ -4,6 +4,7 @@ public class ReverseNo {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter the number : ");
         int num = sc.nextInt();
         int n = num;
@@ -24,6 +25,5 @@ public class ReverseNo {
 
         System.out.println("the reverse of the number using for-loop is " + r);
         sc.close();
-
     }
 }
